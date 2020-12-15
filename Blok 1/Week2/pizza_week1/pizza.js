f424de3f43172761f69f476224254335696c32e9
Raaf <99051006@mydavinci.nl>
2020-12-15 14:17:23 +0100

    pizza code fixed on the right way no document.write

--- Blok 1/Week2/pizza_week1/pizza.js
+++ Blok 1/Week2/pizza_week1/pizza.js
@@ -1,19 +1,15 @@
       // de code waar mensen kunnen zeggen hoeveel pizza"s ze wilen
-          var v_kleinpizza = prompt("hoeveel kleine pizza's wil je hebben?");
-          var v_mediumpizza = prompt("hoeveel medium pizza's wil je hebben?");
-          var v_grootpizza = prompt("hoeveel grootte pizza's wil je hebben?");
-
+          var kleinPizza = prompt("hoeveel kleine pizza's wil je hebben?");
+          var mediumPizza = prompt("hoeveel medium pizza's wil je hebben?");
+          var grootPizza = prompt("hoeveel grootte pizza's wil je hebben?");
           // de prijs per soort pizza
-          const c_klein = 5 ;
-          const c_medium = 7 ;
-          const c_groot = 10 ;
+          const prijsKlien = 5 ;
+          const prijsMedium = 7 ;
+          const prijsGroot = 10 ;
           //hoe hij het geld op telt 
-          document.write ("aantal kleine pizza's: " + v_kleinpizza)
-          document.write ('<br> prijs klein: &euro; ' + v_kleinpizza * c_klein )
-          document.write ("<br><br>aantal medium pizza's: " + v_mediumpizza)
-          document.write ('<br> prijs medium: &euro; ' + v_mediumpizza * c_medium )
-          document.write ("<br><br>aantal grootte pizza's: " + v_grootpizza)
-          document.write ('<br> prijs groot: &euro; ' + v_grootpizza * c_groot )
-          // de totalen kosten
-          var v_totaal =  v_kleinpizza * c_klein + v_mediumpizza * c_medium + v_grootpizza * c_groot;
-          document.write ('<br> <hr> totaal prijs: &euro; ' + v_totaal )
+          console.log("aantal kleine pizza's: " + kleinPizza + "," + " prijs kleine pizza's: " + kleinPizza * prijsKlien + " euro")
+          console.log("aantal medium pizza's: " +  mediumPizza + "," + " prijs medium pizza's: " + mediumPizza * prijsMedium + " euro")
+          console.log("aantal grote pizza's: " +  grootPizza + "," + " prijs grote pizza's: " + grootPizza * prijsGroot + " euro")
+         // de totalen kosten
+          var totaalBedrag =  kleinPizza * prijsKlien + mediumPizza * prijsMedium + grootPizza * prijsGroot;
+          console.log('totaal prijs: ' + totaalBedrag + " euro" )
