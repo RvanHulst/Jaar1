ea012f9a601fbc46996bc34a177122517259a40e
Raaf <99051006@mydavinci.nl>
2021-01-18 23:33:24 +0100

    Code verandererd horeca app naar werkend en duidelijker code

--- week5/horeca.js
+++ week5/horeca.js
@@ -1,75 +1,81 @@
-//prijs en variabelen van de producten van de site
-//var prijsfris = 0;
-//var prijswijn = 0;
-//var prijsbier = 0;
-var prijsbal = 0;
-var prijsf = 1.97;
-var prijsw = 3.15;
-var prijsb = 0.83;
-var prijst = 0.08;
-var hoev_fris = 0;
-var hoev_bier = 0;
-var hoev_wijn = 0;
-var hoev_bal = 0;
-var totaal = 0;
+var frisAmount = 0;
+var frisPrice = 1.50;
 
-//vraag naar bitterballen
-function vraagBitterbalen() {
-  var balhoeveelheid = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?");
+var bierAmount = 0;
+var bierPrice = 2.50;
 
-  if (balhoeveelheid == 8 || balhoeveelheid == 16) {
-   var schaal = prompt("Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?");
-   if (schaal > 8 || schaal < 16) {
-     hoev_bal = hoev_bal + Number(balhoeveelheid) * schaal;
-     prijsbal = prijsbal + (balhoeveelheid * schaal * prijst);
-     console.log(prijsbal);
-     console.log(hoev_bal);
-     vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-   } else {
-    alert("U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.");
-    vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-   }
-  } else {
-    alert("U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.");
-    vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-  }
+var wijnAmount = 0;
+var wijnPrice = 3;
+
+var BitterbalAmount = 0;
+var bitterbalPrice = 0.25;
+
+var currentorder = "none";
+start();
+
+//start het process van het bestellen
+function start() {
+	var order = prompt("Welke bestelling wilt u toevoegen?");
+	if (order.toLowerCase() == "fris" || order.toLowerCase() == "bier" || order.toLowerCase() == "wijn") {
+		currentorder = order;
+		startDrinkAmount();
+	} else if (order.toLowerCase() == "snack") {
+		startSnackAmount();
+
+	} else if (order.toLowerCase() == "stop") {
+		var totaalPrijs = (frisAmount * frisPrice) + (bierAmount * bierPrice) + (wijnAmount * wijnPrice) + (BitterbalAmount * bitterbalPrice);
+		if (frisAmount > 0) {
+			console.log(frisAmount + " fris " + "<br>" + "1 flesje fris kost: €" + frisPrice + "<br>" + "totaal voor fris: " + "€" + frisAmount * frisPrice);
+		}
+		if (bierAmount > 0) {
+			console.log(bierAmount + " bier " + "<br>" + "1 flesje bier kost: €" + bierPrice + "<br>" + "totaal voor bier: " + "€" + bierAmount * bierPrice);
+		}
+		if (wijnAmount > 0) {
+			console.log(frisAmount + " wijn " + "<br>" + "1 glas wijn kost: €" + wijnPrice + "<br>" + "totaal voor wijn: " + "€" + wijnAmount * wijnPrice);
+		}
+		if (BitterbalAmount > 0) console.log("snack, hoeveelheid: " + BitterbalAmount + "<br>" + "toaal voor snacks: " + "€" + BitterbalAmount * bitterbalPrice);
+		console.log("Totaal:" + " € " + totaalPrijs.toFixed(2))
+
+	} else {
+		alert("Dit staat niet in ons menu!")
+		start();
+	}
 }
-//vraag naar naam en hoeveelheid van drank
-function vraagdrank(naam) {
-  if (naam == "fris") {
-    var frishoeveelheid = prompt("Hoeveel " + naam + " wilt u toevoegen aan uw bestelling?");
-    hoev_fris = hoev_fris + Number(frishoeveelheid);
-    prijsfris = prijsfris + (Number(frishoeveelheid) * prijsf);
-  console.log(prijsfris);
-  console.log(hoev_fris);
-    vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-    } else if (naam == "wijn") {
-  var wijnhoeveelheid = prompt("Hoeveel " + naam + " wilt u toevoegen aan uw bestelling?");
-    hoev_wijn = hoev_wijn + Number(wijnhoeveelheid);
-    prijswijn = prijswijn + (Number(wijnhoeveelheid) * prijsw);
-  console.log(prijswijn);
-  vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-    } else if (naam == "bier") {
-  var bierhoeveelheid = prompt("Hoeveel " + naam + " wilt u toevoegen aan uw bestelling?");
-  hoev_bier = hoev_bier + Number(bierhoeveelheid);
-  prijsbier = prijsbier + (Number(bierhoeveelheid) * prijsb);
-  console.log(prijsbier);
-  vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-    } else if (naam == "snack"){
-      
-    vraagBitterbalen();
+//hier word gevraagd hoeveel fris, bier of wijn je wilt bestellen
+function startDrinkAmount() {
+	parseInt();
+	var drinkAmount = parseInt(prompt("Hoeveel " + currentorder + " wil je bestellen?"));
+	if (isNaN(drinkAmount)) {
+		alert("Je moet een getal gebruiken!");
+		startDrinkAmount();
+	} else {
+		if (currentorder.toLowerCase() == "fris") {
+			frisAmount += drinkAmount;
+		} else if (currentorder.toLowerCase() == "bier") {
+			bierAmount += drinkAmount;
+		} else if (currentorder.toLowerCase() == "wijn") {
+			wijnAmount += drinkAmount;
+		}
+		start();
+	}
 
-} else if (naam == null){
-
-return prijsfris, prijswijn, prijsbier, prijsbal, hoev_fris, hoev_bier, hoev_wijn, hoev_bal;
-
-} else {
-  alert("U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.");
-  vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
 }
+//hier word gevraagd hoeveel bitterballen je wilt bestellen
+function startSnackAmount() {
+	var snackBitterballAmount = prompt("Hoeveel bitterballen wil je bestellen?");
+	if (isNaN(parseInt(snackBitterballAmount))) {
+		alert("Je moet een getal gebruiken!");
+		startSnackAmount();
+	}
+	if (snackBitterballAmount == 8) {
+		BitterbalAmount += 8;
+		start();
+	} else if (snackBitterballAmount == 16) {
+		BitterbalAmount += 16;
+		start();
+	} else {
+		alert("Je kan alleen kiezen uit 8 of 16 bitterballen");
+		startSnackAmount();
+	}
+
 }
-
-vraagdrank(prompt("Welke bestelling wilt u toevoegen?"));
-
-//bereken totaal bedrag
-totaal = prijsfris + prijsbier + prijswijn + prijsbal;
