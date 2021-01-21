6bce144ed593b50a2b6efa1e7c61ac6a7e2d4be5
Raaf <99051006@mydavinci.nl>
2021-01-21 21:59:02 +0100

    Adventure Game  starting
    *audio added
    *images added
    *html added 
    *css added
    *js added

---
+++ Adventure_Game/adventure.js
@@ -0,0 +1,15 @@
+var gameContainer = document.getElementById('game-container');
+
+var title = document.getElementById('title');
+var description = document.getElementById('description');
+
+var gameButtons = document.getElementById('game-buttons');
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+
+var inventoryItem = document.getElementById('inventoryItem');
+
+var startMusic = new Audio("audio/");
+var deathSound = new Audio("audio/");
+var winMusic = new Audio("audio/");
