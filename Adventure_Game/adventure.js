4d1b64404f498b7ec4c4c18714e1521d0a452c97
Raaf <99051006@mydavinci.nl>
2021-02-03 12:02:54 +0100

    changes on feedback and added some new gifs, audio, images

--- Adventure_Game/adventure.js
+++ Adventure_Game/adventure.js
@@ -15,24 +15,21 @@ const startMusic = new Audio("audio/riot.mp3");
 const deathSound = new Audio("audio/dead.mp3");
 const winMusic = new Audio("audio/");
 
-alert("Voor de beste ervaring moet u de muziek aanzetten. Klik op de muziek knop.");
-
 function startscreen() {
 	console.log("Start Screen");
 	gameImage.src="images/politielogo.png";
 	title.innerHTML="World War Now";
+	button2.style.display="initial";
+	button3.style.display="initial";
 	button3.onclick=function() {music();};
 	button3.innerHTML="Muziek";
-	button2.onclick=function() {
-		level1();
-	}
+	button2.onclick=level1;
 	button2.innerHTML="Start het spel";
 	button1.style.display="none";
-	description.innerHTML="Welkom bij mij spel. Je gaat nu als een agent spelen in de politie tegen de staats mogolen die gaan rellen";
+	description.innerHTML="Welkom bij het politie spel. Je gaat nu als een agent spelen en je zult de juiste keuzens moeten maken want alles wat je doet kan ook tegen je gebruikt worden";
 	description.style.width="500px";
 	description.style.margin="0 auto";
 }
-
 startscreen();
 
 function music() {
@@ -46,8 +43,13 @@ function death() {
 	console.log("Death");
 	deathSound.play();
 	deathSound.volume = 1.0;
-	alert("Je hebt de verkeerde keuze gemaakt. Je bent dood. Je kunt opnieuw beginnen.");
-	startscreen();
+	gameImage.src="images/death.jpg";
+	title.innerHTML="Retry";
+	description.innerHTML=("Je hebt de verkeerde keuze gemaakt. Je bent dood. Je kunt opnieuw beginnen.");
+	button1.style.display="none";
+	button2.style.display="none";
+	button3.style.display="none";
+	setTimeout(startscreen,5000)
 }
 
 function itemAlert() {
@@ -60,15 +62,12 @@ function level1() {
 	gameImage.src="images/briefing.png"
 	title.innerHTML="De Briefing";
 	description.innerHTML="je krijgt uitleg en er wordt verteld dat er vanavond rellen zullen zijn. iedreen moet naar locatie b5ad gaan oftewel zuidplein Roterdam";
-	button2.onclick=function() {
-		death();
-	}
+	button2.onclick=death;
 	button2.innerHTML="je neemt je geweer in je mond en vraagt jezelf af als die geladen is.";
-	button3.onclick=function() {
-		level2();
-	}
+	button3.onclick=level2;
 	button3.innerHTML="Je hebt goed opgelet en gaat naar de locatie dat werd gemeld.";
 }
+
 function level2() {
 	console.log("Level 2");
 	gameImage.src="images/locatie1.jpg"
@@ -76,39 +75,54 @@ function level2() {
 	description.innerHTML="je bent op de locatie van het incident,";
 	button2.onclick=verkeerdeKeuzen1;
 	button2.innerHTML="je doet niks en loopt weg van de locatie .";
-	button3.onclick=function() {
-		level3();
-	}
+	button3.onclick=level3;
 	button3.innerHTML="je doet iets.";
 }
+
 function level3() {
 	console.log("Level 3");
 	gameImage.src="images/"
 	title.innerHTML="Verdachte situatie";
 	description.innerHTML="je ziet een groep hang jongeren mensen irriteren en grof taal gebruik aan het uiten naar omstanders in de buurt,";
-	button2.onclick=function() {
-		verkeerdeKeuzen1();
-	}
+	button2.onclick=verkeerdeKeuzen1;
 	button2.innerHTML="je doet niks en loopt weg van de locatie .";
-	button3.onclick=function() {
-		level4();
-	}
+	button3.onclick=level4;
 	button3.innerHTML="je doet iets.";
 }
+
 function level4() {
 	console.log("Level 4");
 	gameImage.src="images/radioPolice.jpg"
 	title.innerHTML="Radio Bericht";
 	description.innerHTML="......KRKAAA...Meldkamer.. Rotterdam-Zuid krrrr.... Rellen starten kkkrrrr... Meerdere eenheden in de buurt.... reageer er op..... CODE BLAUW.. CODE BLAUW. (dit bericht vervolgt nog 4x)";
-	button2.onclick=function() {
-		death();
-	}
+	button2.onclick=death;
 	button2.innerHTML="je doet niks en loopt weg van de locatie die op de radio was.";
-	button3.onclick=function() {
-		level5();
-	}
+	button3.onclick=level5;
 	button3.innerHTML="je reageerd naar de call locatie.";
 }
+
+function level5() {
+	console.log("Level 5");
+	gameImage.src="images/.jpg"
+	title.innerHTML="level5";
+	description.innerHTML="description";
+	button2.onclick=death;
+	button2.innerHTML="dood keuzen";
+	button3.onclick=level6;
+	button3.innerHTML="volgende level";
+}
+
+function level6() {
+	console.log("Level 6");
+	gameImage.src="images/.jpg"
+	title.innerHTML="level6";
+	description.innerHTML="";
+	button2.onclick=alternatievenKeuzen2;
+	button2.innerHTML="alternatieven Keuzen 2";
+	button3.onclick=functionlevel7;
+	button3.innerHTML="volgende level";
+}
+
 
 
 
@@ -142,13 +156,9 @@ function verkeerdeKeuzen1() {
 	gameImage.src="images/Minecraft.jpg"
 	title.innerHTML="De verkeerde Keuzen test";
 	description.innerHTML="gok tussen het getal 1 of 2, kies de juiste en ga verder kies de verkeerde en ga dood!";
-	button2.onclick=function() {
-		death();
-	}
+	button2.onclick=death;
 	button2.innerHTML="Getal 1";
-	button3.onclick=function() {
-		death();
-	}
+	button3.onclick=death;
 	button3.innerHTML="Getal 2";
 }
 
